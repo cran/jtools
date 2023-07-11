e76bd055495df44c8079f0128c8eb9360659cc3a
Jacob A. Long <jacob.long@sc.edu>
2023-07-11 08:40:29 +0000

    version 2.2.2

--- inst/doc/svycor.R
+++ inst/doc/svycor.R
@@ -6,45 +6,45 @@ if (!all(sapply(required, requireNamespace, quietly = TRUE)))
 library(jtools)
 
 ## -----------------------------------------------------------------------------
-library(survey)
-data(api)
-dstrat <- svydesign(id = ~1,strata = ~stype, weights = ~pw, data = apistrat, fpc=~fpc)
+#  library(survey)
+#  data(api)
+#  dstrat <- svydesign(id = ~1,strata = ~stype, weights = ~pw, data = apistrat, fpc=~fpc)
 
 ## -----------------------------------------------------------------------------
-svycor(~api00 + api99, design = dstrat)
+#  svycor(~api00 + api99, design = dstrat)
 
 ## -----------------------------------------------------------------------------
-svycor(~api00 + api99, design = dstrat, digits = 4)
+#  svycor(~api00 + api99, design = dstrat, digits = 4)
 
 ## -----------------------------------------------------------------------------
-svycor(~api00 + api99, design = dstrat, digits = 4, sig.stats = TRUE, bootn = 2000, mean1 = TRUE)
+#  svycor(~api00 + api99, design = dstrat, digits = 4, sig.stats = TRUE, bootn = 2000, mean1 = TRUE)
 
 ## -----------------------------------------------------------------------------
-c <- svycor(~api00 + api99, design = dstrat, digits = 4, sig.stats = TRUE, bootn = 2000, mean1 = TRUE)
-
-c$cors
-
-c$p.values
-
-c$std.err
-
-## -----------------------------------------------------------------------------
-svyvar(~api00 + api99, design = dstrat)
+#  c <- svycor(~api00 + api99, design = dstrat, digits = 4, sig.stats = TRUE, bootn = 2000, mean1 = TRUE)
+#  
+#  c$cors
+#  
+#  c$p.values
+#  
+#  c$std.err
 
 ## -----------------------------------------------------------------------------
-var <- svyvar(~api00 + api99, design = dstrat)
-var <- as.matrix(var)
-var
+#  svyvar(~api00 + api99, design = dstrat)
 
 ## -----------------------------------------------------------------------------
-cor <- cov2cor(var)
-cor
+#  var <- svyvar(~api00 + api99, design = dstrat)
+#  var <- as.matrix(var)
+#  var
 
 ## -----------------------------------------------------------------------------
-cor <- cor[1:nrow(cor), 1:nrow(cor)]
-cor
+#  cor <- cov2cor(var)
+#  cor
 
 ## -----------------------------------------------------------------------------
-out <- svycor(~api99 + api00, design = dstrat)
-out$cors
+#  cor <- cor[1:nrow(cor), 1:nrow(cor)]
+#  cor
+
+## -----------------------------------------------------------------------------
+#  out <- svycor(~api99 + api00, design = dstrat)
+#  out$cors
 
